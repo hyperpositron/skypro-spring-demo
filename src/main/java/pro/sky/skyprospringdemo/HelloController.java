c2f9d55093dc91f8b7b533cc9ace2be126e427a5
Александр <hyperpositron@gmail.com>
2023-03-15 22:34:24 +0200

    создание GET Mapping приветствие по имени

--- src/main/java/pro/sky/skyprospringdemo/HelloController.java
+++ src/main/java/pro/sky/skyprospringdemo/HelloController.java
@@ -1,6 +1,8 @@
 package pro.sky.skyprospringdemo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +10,13 @@ public class HelloController {
     @GetMapping
     public String hello(){
         return "hello";
+    }
+//    @GetMapping(path = "/hello")
+//    public String answerHello(){
+//        return "<b>hello</b>";
+//    }
+    @GetMapping(path = "/hello")
+    public String answerHello(@RequestParam("name")String userName){
+        return "<b>hello </b>" + userName;
     }
 }
