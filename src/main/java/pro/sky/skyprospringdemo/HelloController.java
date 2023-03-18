6e1da1133af6b3e01352ba7e1ecae683af21a426
Александр <hyperpositron@gmail.com>
2023-03-18 10:37:29 +0200

    комменты

--- src/main/java/pro/sky/skyprospringdemo/HelloController.java
+++ src/main/java/pro/sky/skyprospringdemo/HelloController.java
@@ -15,12 +15,12 @@ public class HelloController {
 //    public String answerHello(){
 //        return "<b>hello</b>";
 //    }
-    @GetMapping(path = "/hello")
+    @GetMapping(path = "/hello")    //http://localhost:8080/hello?name=alan
     public String answerHello(@RequestParam("name")String userName){
         return "<b>hello </b>" + userName;
     }
 
-    @GetMapping(path = "/vam_suda")
+    @GetMapping(path = "/vam_suda") //http://localhost:8080/vam_suda
     public String Ere() {
         return "<b>куда?? </b>";
     }
